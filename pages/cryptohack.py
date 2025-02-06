13e8de9d33979b89b23cb9e037796b4b0750e67f
ᐖ . <d@Bonjour.local>
2025-02-06 12:51:46 +0800

    comm

---
+++ pages/cryptohack.py
@@ -0,0 +1,8 @@
+# import base64 
+from base64 import b64decode 
+  
+s = b'72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf'
+# Using base64.b64decode() method 
+gfg = b64decode(s) 
+  
+print(gfg) 
